43a4a5ebf7724bef667205f62a16db542cfa87ed
razajp <hasanraza03165825495@gmail.com>
2026-08-29 18:13:44 +0500

    add article category (romper) with single part (romper)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -46,6 +46,7 @@ class AppServiceProvider extends ServiceProvider
                     '1_pc_koti' => ['text' => '1 Pc + Koti'],
                     '2_pc' => ['text' => '2 Pc'],
                     '3_pc' => ['text' => '3 Pc'],
+                    'romper' => ['text' => 'Romper'],
                 ],
                 'seasons' => [
                     'half' => ['text' => 'Half'],
@@ -73,16 +74,19 @@ class AppServiceProvider extends ServiceProvider
                     '1_pc_koti_half' => ['shirt', 'koti'],
                     '2_pc_half' => ['shirt', 'neker'],
                     '3_pc_half' => ['koti', 'inner', 'neker'],
+                    'romper_half' => ['romper'],
                     '1_pc_full' => ['shirt'],
                     '1_pc_inner_full' => ['shirt', 'inner'],
                     '1_pc_koti_full' => ['shirt', 'koti'],
                     '2_pc_full' => ['shirt', 'trouser'],
                     '3_pc_full' => ['koti', 'inner', 'neker'],
+                    'romper_full' => ['romper'],
                     '1_pc_winter' => ['shirt'],
                     '1_pc_inner_winter' => ['shirt', 'inner'],
                     '1_pc_koti_winter' => ['shirt', 'koti'],
                     '2_pc_winter' => ['shirt', 'trouser'],
                     '3_pc_winter' => ['koti', 'inner', 'trouser'],
+                    'romper_winter' => ['romper'],
                 ],
             ];
         });
